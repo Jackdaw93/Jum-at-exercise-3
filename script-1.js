5d4a7db3386ec5acf4a34cba3420d983d5b00d66
Jackdaw93 <rohmad37@ymail.com>
2020-06-12 20:36:54 +0700

    Revisi Logika

--- script-1.js
+++ script-1.js
@@ -1,14 +1,17 @@
 let age = prompt("Masukan Umur");
 
 const kucing = (age) => {
-  if (age === 1) {
-    return `Umur kucing 15 ali umur manusia`;
-  } else if (age === 2) {
+  if (age == 1) {
+    return `Umur kucing 15 umur manusia`;
+  } else if (age == 2) {
     return `Umur kucing 24 kali umur manusia`;
   } else if (age >= 3) {
-    return `Umur kucing 32 kali umur manusia`;
+    let catAge = 24 + (age - 2) * 4;
+    return `Umur kucing ${catAge} umur manusia`;
+  } else {
+    return `Kosong`;
   }
 };
 
-document.write(kucing(age))
+document.write(kucing(age));
 console.log(kucing(age));
